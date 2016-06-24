7a33ec8724775e4d3a5454164610150ab107cec4
honza.pofider@seznam.cz <honza.pofider@seznam.cz>
2016-06-24 12:41:32 +0200

    install jsreport with latest tag

--- bin/start.js
+++ bin/start.js
@@ -8,7 +8,7 @@ try {
   fs.statSync(path.join(process.cwd(), 'node_modules', 'jsreport'))
 } catch (e) {
   console.log('Installing the latest jsreport, this takes few minutes')
-  execSync('npm install jsreport@next', { stdio: [0, 1, 2] })
+  execSync('npm install jsreport', { stdio: [0, 1, 2] })
 }
 
 function tryRequire (module) {
