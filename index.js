442664e0d725561588d5b56efec1ea5f9f002a67
BJR Matos <bjrmatos@gmail.com>
2017-04-10 14:02:28 -0500

    export dependencies

---
+++ index.js
@@ -0,0 +1,8 @@
+
+module.exports = {
+  deps: {
+    webpack: require('webpack'),
+    autoprefixer: require('autoprefixer'),
+    'postcss-flexbugs-fixes': require('postcss-flexbugs-fixes')
+  }
+}
