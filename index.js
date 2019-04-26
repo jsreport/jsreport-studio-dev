b900b335645f185013f6d6da21a7482ed4db0832
BJR Matos <bjrmatos@gmail.com>
2019-04-26 18:08:22 -0500

    add html-webpack-plugin, clean-webpack-plugin to common deps

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ module.exports = {
   deps: {
     webpack: require('webpack'),
     autoprefixer: require('autoprefixer'),
-    'postcss-flexbugs-fixes': require('postcss-flexbugs-fixes')
+    'postcss-flexbugs-fixes': require('postcss-flexbugs-fixes'),
+    'html-webpack-plugin': require('html-webpack-plugin'),
+    'clean-webpack-plugin': require('clean-webpack-plugin')
   }
 }
